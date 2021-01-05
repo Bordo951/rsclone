550cbfd4d33edbf052163ebcf4aa5c958bb0ed4c
Angelika <kozlovaangelika09@gmail.com>
2021-01-05 23:02:46 +0300

    EP-2 Create humburger menu feat: fix bugs

--- src/js/initializers/events-initializer.js
+++ src/js/initializers/events-initializer.js
@@ -1,11 +1,8 @@
-//import ClickOnZoomEvent from "../events/click-on-zoom";
-
 import ClickOnBurgerEvent from "../events/click-on-burger";
 
 export default class EventsInitializer {
     constructor() {
         this.eventsPool = [
-            //new ClickOnZoomEvent()
             new ClickOnBurgerEvent(),
         ]
     }
