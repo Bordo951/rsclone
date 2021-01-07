0e3f01fab88f2cc99f641397f07e90bd7fa5345c
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-01-07 05:49:56 +0300

    EP-4: Add the Task List page
    feat: add list page

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -38,10 +38,16 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
+      title: 'Tasks matrix',
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/index.html', // template file
       filename: 'index.html', // output file
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Task list',
+      favicon: paths.src + '/images/favicon.png',
+      template: paths.src + '/task-list.html', // template file
+      filename: 'task-list.html', // output file
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
