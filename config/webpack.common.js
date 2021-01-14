b67a3b1dec292d6e860cdaf76a0eb03471f7e885
Anastasiya Fatykhava <fatykhava@yandex.ru>
2021-01-14 20:48:44 +0300

    EP-6: Create goal tracker page
    init: start task
    
    feat: add chart and tracker

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -55,6 +55,12 @@ module.exports = {
       template: paths.src + '/about.html', // template file
       filename: 'about.html', // output file
     }),
+    new HtmlWebpackPlugin({
+      title: 'Tracker',
+      favicon: paths.src + '/images/favicon.png',
+      template: paths.src + '/tracker.html', // template file
+      filename: 'tracker.html', // output file
+    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
