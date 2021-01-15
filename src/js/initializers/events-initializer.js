14cdd5a541c18fec002f0b5145860780890b4ba9
Angelika <kozlovaangelika09@gmail.com>
2021-01-15 21:38:10 +0300

    EP-27 Add functionality to change the theme Feat: Add functionality to change the theme

--- src/js/initializers/events-initializer.js
+++ src/js/initializers/events-initializer.js
@@ -1,6 +1,7 @@
-import ClickOnPopup from "../events/click-on-popup";
-import ClickOnBurgerEvent from "../events/click-on-burger";
-import ClickChangeLanguage from "../events/click-change-language";
+import ClickOnPopup from '../events/click-on-popup';
+import ClickOnBurgerEvent from '../events/click-on-burger';
+import ClickChangeLanguage from '../events/click-change-language';
+import ClickChangeTheme from '../events/click-change-theme';
 
 export default class EventsInitializer {
     constructor() {
@@ -8,12 +9,13 @@ export default class EventsInitializer {
             new ClickOnPopup(),
             new ClickOnBurgerEvent(),
             new ClickChangeLanguage(),
+            new ClickChangeTheme(),
         ]
     }
 
     initEvents() {
         this.eventsPool.forEach((item) => {
-            document.addEventListener("DOMContentLoaded", item.initEvent());
+            document.addEventListener('DOMContentLoaded', item.initEvent());
         });
     }
 }
