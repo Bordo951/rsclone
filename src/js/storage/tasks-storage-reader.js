0803207f784c0d06390b7c590f6ee69e5c254547
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-01-17 23:05:01 +0300

    EP-19: Add functionality for calculating the total number of tasks
    fix: fixed display of tasks on the board depending on the progress status: completed / not completed

--- src/js/storage/tasks-storage-reader.js
+++ src/js/storage/tasks-storage-reader.js
@@ -2,24 +2,24 @@ export default class TasksStorageReader {
     getImportantUrgent() {
         const savedTasks = JSON.parse(localStorage.getItem('_tasks')) ?? [];
 
-        return savedTasks.filter(task => task.isImportant && task.isUrgently)
+        return savedTasks.filter(task => task.isImportant && task.isUrgently && !task.isCompleted)
     }
 
     getImportantNotUrgent() {
         const savedTasks = JSON.parse(localStorage.getItem('_tasks')) ?? [];
 
-        return savedTasks.filter(task => task.isImportant && !task.isUrgently)
+        return savedTasks.filter(task => task.isImportant && !task.isUrgently && !task.isCompleted)
     }
 
     getNotImportantUrgent() {
-        const savedTasks = JSON.parse(localStorage.getItem('_tasks'))?? [];
+        const savedTasks = JSON.parse(localStorage.getItem('_tasks')) ?? [];
 
-        return savedTasks.filter(task => !task.isImportant && task.isUrgently)
+        return savedTasks.filter(task => !task.isImportant && task.isUrgently && !task.isCompleted)
     }
 
     getNotImportantNotUrgent() {
-        const savedTasks = JSON.parse(localStorage.getItem('_tasks'))?? [];
+        const savedTasks = JSON.parse(localStorage.getItem('_tasks')) ?? [];
 
-        return savedTasks.filter(task => !task.isImportant && !task.isUrgently)
+        return savedTasks.filter(task => !task.isImportant && !task.isUrgently && !task.isCompleted)
     }
 }
