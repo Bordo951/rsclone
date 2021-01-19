101c4a01e5a40ebb3492de89e6d87cdafb9482fb
Anastasiya Fatykhava <fatykhava@yandex.ru>
2021-01-19 22:10:23 +0300

    EP-36: Scroll the habits by clicking on arrows
    init: start task
    
    feat: add showTracker function

--- src/js/initializers/tracker-initializer.js
+++ src/js/initializers/tracker-initializer.js
@@ -6,7 +6,7 @@ export default class TrackerInitializer {
   }
 
   init() {
-    this.showTrackers();
+    this.showTrackers(0);
     this.showChart();
   }
 
@@ -15,7 +15,32 @@ export default class TrackerInitializer {
     this.chart.createChart(percentages);
   }
 
-  showTrackers() {
-    console.log('tracker');
+  showTrackers(num) {
+    let trackerArrs = [];
+
+    if (!!localStorage.getItem('_trackers')) {
+      trackerArrs = JSON.parse(localStorage.getItem('_tracker'));
+    } else {
+      for (let i = 0; i < 6; i++) {
+        const trackerArr = [];
+        for (let j = 0; j < 30; j++) {
+          trackerArr.push(0);
+        }
+        trackerArrs.push(trackerArr);
+      }
+    }
+
+    const trackerContainer = document.querySelector('.tracker__checkbox');
+    trackerContainer.innerHTML = '';
+
+    trackerArrs[num].forEach((elem) => {
+      const div = document.createElement('div');
+      if (elem) {
+        div.className = 'tracker__day done';
+      } else {
+        div.className = 'tracker__day';
+      }
+      trackerContainer.append(div);
+    })
   }
 }
